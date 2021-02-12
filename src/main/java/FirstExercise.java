1b99b57197ef89192567b02bab5785bb54e5e8ee
JohnnyLFisher <johnny.luel.fisher@gmail.com>
2021-02-12 11:00:52 -0600

    First Exercise Complete

--- src/main/java/FirstExercise.java
+++ src/main/java/FirstExercise.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
+import static org.apache.commons.lang3.StringUtils.*;
+
 public class FirstExercise {
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Please enter a sentence:");
+		String str = input.nextLine();
+		System.out.println("You entered: " + str);
+		if (isNumeric(str)){
+			System.out.println("\"" + str + "\"" + " is a number.");
+		} else {
+			System.out.println("\"" + str + "\"" + " is not a number.");
+			System.out.println("Flipped Case: " + swapCase(str));
+		}
+		System.out.println("Reversed: " + reverse(str));
 	}
 }
