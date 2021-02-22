47613403f56d053d1b9e446e123ec09f7792fc55
JohnnyLFisher <johnny.luel.fisher@gmail.com>
2021-02-22 09:55:20 -0600

    JUnit Warmup Complete

---
+++ src/test/java/MyFirstTest.java
@@ -0,0 +1,39 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class MyFirstTest {
+
+	@Test
+	public void testIfCodeupIsSpelledRight(){
+		assertEquals("Codeup", "Codeup");
+	}
+
+	@Test
+	public void testIfArrayListIsTheSame(){
+		List<String> languages = new ArrayList<>();
+		List<String> moreLanguages = new ArrayList<>();
+		assertNotSame(languages, moreLanguages);
+	}
+
+	@Test
+	public void testIfArraysAreEqual(){
+		int[] numbers = {1, 2, 3};
+		int[] otherNumbers = new int[3];
+		otherNumbers[0] = 1;
+		otherNumbers[1] = 2;
+		otherNumbers[2] = 3;
+		assertArrayEquals(numbers, otherNumbers);
+	}
+
+	@Test
+	public void testIfContainsWorks(){
+		String language = "PHP";
+		assertTrue(language.contains("H")); // use assertTrue for this statement
+		assertFalse(language.contains("J")); // use assertFalse for this statement
+	}
+
+}
